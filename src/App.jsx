d279bdb25a62475afba6b69914ebeab6d505bf10
Lesia_Mandziuk <lesia.mandziuk@gmail.com>
2022-01-12 13:33:54 +0200

    merge branches

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
-import Header from './Redux/Components/Structure/header';
-import Footer from './Redux/Components/Structure/footer';
+import Home from './components/home';
+import About from './components/about';
+import Navbar from './components/structure/navbar';
 
 function App() {
   return (
-    <div className="Conteiner">
-      <Router>
-        <Header />
-        <h1>Події в твоєму місті </h1>
-        <h2>Заліщики</h2>
-        <aside>
-          <h2>Aside Header</h2>
-          <p>no facebook</p>
-          <p>
-            facebook{' '}
-            <a href="https://www.facebook.com/Zalishchykyturystychni">
-              Inline Link
-            </a>
-          </p>
-        </aside>
-        <Footer />
-      </Router>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
